473cd6a9e17682e8863e9c537f55f68161f5f174
Alice Hilary Zheng <alicehzheng@gmail.com>
2016-10-12 16:19:02 +0800

    1.Fixed a bug at SearchNote() function in Notebook class 2.Implemented several test cases

--- src/base/TextNote.java
+++ src/base/TextNote.java
@@ -1,14 +1,11 @@
 package base;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.io.FileOutputStream;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+
 
 public class TextNote extends Note implements java.io.Serializable{
 	private String content;
@@ -80,6 +77,29 @@ public class TextNote extends Note implements java.io.Serializable{
 	}
 	public String getContent(){
 		return this.content;
+	}
+	
+	//for test
+	public Character countLetters(){
+		HashMap<Character,Integer> count = new HashMap<Character,Integer>();
+		String a = this.getTitle() + this.getContent();
+		int b = 0;
+		Character r = ' ';
+		for (int i = 0; i < a.length(); i++) {
+			Character c = a.charAt(i);
+			if (c <= 'Z' && c >= 'A' || c <= 'z' && c >= 'a') {
+				if (!count.containsKey(c)) {
+					count.put(c, 1);
+				} else {
+					count.put(c, count.get(c) + 1);
+					if (count.get(c) > b) {
+						b = count.get(c);
+						r = c;
+					}
+				}
+			}
+		}
+		return r;
 	}
 	
 }
